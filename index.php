a628ef25b7919e8dde7068ad062a3e636713dda3
EttBraNamn <36034854+EttBraNamn@users.noreply.github.com>
2018-09-17 10:51:57 +0200

    Start på index

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
-<?php
+<!DOCTYPEHTML>
+<html lang="sv">
+	<head>
+		<meta  charset="UTF-8"/>
+		<title>Login</title>
+	</head>
+	<?php
 
-echo("AHH");
-echo("php");
-echo("AA");
-echo("IGE");
-echo("AH");
-echo("HH");
-echo("KYS");
-echo("EttTill");
-echo("AH");
-?>
+	function PrintForm()
+	{
+		echo("<form action=\"\" method=\"POST\">
+			<input type=\"text\" value=\"Lägenhetsnummer\"/ name=\"appartment\">
+			<input type=\"password\" value=\"Password\" name=\"password\"/>
+			<input type=\"submit\"/>
+			</form>");
+	}
+		echo("<body>");
+		
+		if (isEmpty($_POST))
+		{
+			PrintForm();
+		}
+		else 
+		{
+			
+
+		}
+}
+
+		
+	?>
+	</body>
+</html>
