db2733f0790036a3e7bbb108703b51b048e874f0
EttBraNamn <36034854+EttBraNamn@users.noreply.github.com>
2018-09-18 11:00:49 +0200

    Flyttade lite till sql.php

--- index.php
+++ index.php
@@ -5,46 +5,11 @@
 		<title>Login</title>
 	</head>
 	<?php
-	include 'globalVal.php'
-	
-	//Function that returns an array requests if succesfull. Otherwise returns ERROR or NOTHING depending
-	function SqlRequest($query, $db)
-	{
-		try
-		{
-			//SERVERNAME USERNAME and PASSWORD are all values defined in globalVal.php
-			$connect = new PDO("mysql:host=" . SERVERNAME . ";dbname=$db", USERNAME, PASSWORD);
 
-			//We do not want to continue if something goes wrong
-			if (!($stmt = $connect->prepare($query))) 			{
-				return ERROR;
-			}
-
-			//We do not want to continue if something goes wrong
-			if(!($stmt->execute()) 
-			{
-				return ERROR;
-			}
-			$toReturn = "";
-			//Checking if there's nothing to return
-			if(!($toReturn = stmt->fetchAll())
-			{
-				return NOTHING;
-			}
-			return $toReturn;
-		}
-		catch
-		{
-			return ERROR;
-		}
-	}
-	//Removing any odd inputs done by the user
-	function CleanString($input)
-	{
-		$input = htmlspecialchars($input);
-		return $input;
-	}
-	
+	//INCLUDES
+	include 'globalVal.php';
+	include 'sql.php';
+		
 	//Prints out the basic loginform
 	function PrintForm($error = false)
 	{
@@ -60,19 +25,25 @@
 	}
 	
 	
-	//START OF GLOBALCODE//
+	/*
+	#####################################
+	--------START OF GLOBAL CODE---------
+	#####################################
+	*/
 	echo("<body>");
 		
 	//If no post call has been made
 	if (isEmpty($_POST))
 	{
-			PrintForm();
+		PrintForm();
 	}
 	else 
 	{
-			$appartment = CleanString($_POST('appartment'));
-			$password = CleanString($_POST('password'));
-	
+			
+		$appartment = CleanString($_POST('appartment'));
+		$password = CleanString($_POST('password'));
+			
+			//SqlRequest from sql.php
 			$result = SqlRequest("SELECT appartment password FROM users", DBUSERS);
 			
 			$curr = false;
@@ -89,6 +60,70 @@
 				return;
 			}
 			
+	}	
+	?>
+	</body>
+</html><!DOCTYPEHTML>
+<html lang="sv">
+	<head>
+		<meta  charset="UTF-8"/>
+		<title>Login</title>
+	</head>
+	<?php
+
+	//INCLUDES
+	include 'globalVal.php';
+	include 'sql.php';
+		
+	//Prints out the basic loginform
+	function PrintForm($error = false)
+	{
+		if ($error)
+		{
+			echo ("<p>Fel lägenhetsnummer eller användarnamn</p>")
+		}
+		echo("<form action=\"\" method=\"POST\">
+			<input type=\"text\" value=\"Lägenhetsnummer\"/ name=\"appartment\">
+			<input type=\"password\" value=\"Password\" name=\"password\"/>
+			<input type=\"submit\"/>
+			</form>");
+	}
+	
+	/*
+	#####################################
+	--------START OF GLOBAL CODE---------
+	#####################################
+	*/
+	echo("<body>");
+		
+	//If no post call has been made
+	if (isEmpty($_POST))
+	{
+		PrintForm();
+	}
+	else 
+	{
+			
+		$appartment = CleanString($_POST('appartment'));
+		$password = CleanString($_POST('password'));
+			
+		//SqlRequest from sql.php
+		$result = SqlRequest("SELECT appartment password FROM users", DBUSERS);
+		
+		$curr = false;
+		for($i = 0; $i < count($result) && !$curr; ++$i)
+		{
+			if ($username == $result[$i] => "appartment")
+			{
+				$curr = $result[$i];
+			}
+		}
+
+		if (!$curr)
+		{
+			PrintForm(true);
+			return;
+		}		
 	}	
 	?>
 	</body>
---
+++ sql.php
@@ -0,0 +1,38 @@
+<?php
+
+//Function that returns an array requests if succesfull. Otherwise returns ERROR or NOTHING depending
+function SqlRequest($query, $db)
+{
+	try
+	{
+		//SERVERNAME USERNAME and PASSWORD are all values defined in globalVal.php
+		$connect = new PDO("mysql:host=" . SERVERNAME . ";dbname=$db", USERNAME, PASSWORD);
+		//We do not want to continue if something goes wrong
+		if (!($stmt = $connect->prepare($query))) 			{
+			return ERROR;
+		}
+		//We do not want to continue if something goes wrong
+		if(!($stmt->execute()) 
+		{
+			return ERROR;
+		}
+		$toReturn = "";
+		//Checking if there's nothing to return
+		if(!($toReturn = stmt->fetchAll())
+		{
+			return NOTHING;
+		}
+		return $toReturn;
+	}
+	catch
+	{
+		return ERROR;
+	}
+}
+//Removing any odd inputs done by the user
+function CleanString($input)
+{
+	$input = htmlspecialchars($input);
+	return $input;
+}
+?>
