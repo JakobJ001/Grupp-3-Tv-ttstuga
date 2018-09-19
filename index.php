013fe9326c8517980f31870927361b11cd2a1b07
EttBraNamn <36034854+EttBraNamn@users.noreply.github.com>
2018-09-19 15:59:28 +0200

    Bättre

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 
-<html lang="sv">
-	<head>
-		<meta charset="UTF-8">
-		<title>Login</title>
-	</head>
+
 	<?php
 	
 	header('Content-type: text/html; charset=utf-8');
@@ -15,6 +11,11 @@
 	//Prints out the basic loginform
 	function PrintForm($error = false)
 	{
+		echo("<html lang=\"sv\">
+			<head>
+			<meta charset=\"UTF-8\">
+			<title>Login</title>
+			</head><body>");
 		if ($error)
 		{
 			echo ("<p>Fel lägenhetsnummer eller användarnamn</p>");
@@ -23,7 +24,7 @@
 			<input type=\"text\" value=\"Lägenhetsnummer\"/ name=\"appartment\">
 			<input type=\"password\" value=\"Password\" name=\"password\"/>
 			<input type=\"submit\"/>
-			</form>");
+			</form></body></html>");
 	}
 	
 	
@@ -32,52 +33,52 @@
 	--------START OF GLOBAL CODE---------
 	#####################################
 	*/
-	echo("<body>");
-		
 	//If no post call has been made
 	if (empty($_POST))
 	{
 		PrintForm();
 		return;
 	}
-			
-	$appartment = CleanString($_POST['appartment']);
-	$password = CleanString($_POST['password']);
-			
-	$rowCount;
-	//SqlRequest from sql.php		
-	$result = SqlRequest("SELECT appartment password FROM users", DBUSERS, $rowCount);
-			
-	$curr = false;
-	for($i = 0; $i < $rowCount && !$curr; ++$i)
-	{
-		$array = mysql_fetch_array($result[i]);
-		if ($username == $result[$i]['username'])
-		{
-			$curr = $result[$i];
-		}
-	}
-	if (!$curr)
-	{
-		PrintForm(true);
-		return;
-	}
-	if (!password_verify($password, $curr['password']))
-	{
-		PrintForm(true);
-		return;
-	}
-	if ($username == "admin")
-	{
-		AdminSHit();
-		return;
-	}
 	else
 	{
-		OtherShit();
-		return;
-	}
+		
+		$appartment = CleanString($_POST['appartment']);
+		$password = CleanString($_POST['password']);
+				
+		$rowCount;
+		//SqlRequest from sql.php		
+		$result = SqlRequest("SELECT appartment password FROM users", DBUSERS, $rowCount);
+				
+		$curr = false;
+		for($i = 0; $i < $rowCount && !$curr; ++$i)
+		{
+			$array = mysql_fetch_array($result[i]);
+			if ($username == $result[$i]['username'])
+			{
+				$curr = $result[$i];
+			}
+		}
+		if (!$curr)
+		{
+			PrintForm(true);
+			return;
+		}
+		if (!password_verify($password, $curr['password']))
+		{
+			PrintForm(true);
+			return;
+		}
+		if ($username == "admin")
+		{
+			AdminSHit();
+			return;
+		}
+		else
+		{
+			OtherShit();
+			return;
+		}
+		
+	}		
 	
 ?>
-</body>
-</html>
