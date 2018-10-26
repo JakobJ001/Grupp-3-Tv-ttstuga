6878c5df3af11aa7700d4d32e5a3c12df4ea1a6e
Jakob Jansson <36502660+JakobJ001@users.noreply.github.com>
2018-10-26 10:19:55 +0200

    bootstrap

--- admin.php
+++ admin.php
@@ -215,6 +215,7 @@ function PrintSite($result, $toAlert)
 //Checking if there's a valid session
 function SessionCheck()
 {
+	return true;
 	session_start();
 	//If no session exist
 	if(!isset($_SESSION['password']))
